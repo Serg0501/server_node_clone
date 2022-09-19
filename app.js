f11865fc121a51ed65d6d9691f1eac24c63922b2
Serg0501 <gavrilenko87sd@gmail.com>
2022-09-19 20:39:11 +0300

    server clone

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const {userController} = require('./controllers');
+const {validate} = require('./middleware');
+
+const app = express();
+app.use(express.json());
+
+// users
+app.get("/users", userController.getAllUsers);
+app.get("/users/:id", userController.getUser);
+
+app.post("/users", validate.validateUserCreate, userController.createNewUser);
+
+app.patch("/users/:id", userController.updateUser);
+
+app.delete("/users/:id", userController.deleteUser);
+
+module.exports = app;
---
+++ controllers/userConroller.js
@@ -0,0 +1,50 @@
+const {Users} = require('./../models');
+
+//crud
+
+module.exports.getAllUsers = (req, res) => {
+    const data = Users.getAllUsers();
+    res.status(200).send(data);
+};
+
+module.exports.getUser = (req, res) => {
+    const {id} = req.params;
+    const foundUser = Users.getUserById(id);
+    if(foundUser){ 
+        res.status(200).send(foundUser); 
+        return; 
+    }
+    res.status(404).send(`User #${id} not found in data base`); 
+};
+
+module.exports.createNewUser = (req, res) => {
+    const { body } = req;
+    const newUser = Users.createUser(body);
+    res.status(201).send(newUser);
+};
+
+module.exports.updateUser = (req, res) => {
+    //const {id} = req.params;
+    //const {body} = req;
+    const {
+        body,
+        params: {id}
+    } = req;
+    const foundUser = Users.updateUser(id, body);
+    if(foundUser){ 
+        res.status(200).send(foundUser); 
+        return; 
+    }
+    res.status(404).send(`User #${id} not found in data base`); 
+};
+
+module.exports.deleteUser = (req, res) => {
+    const {id} = req.params;
+    const foundUser = Users.deleteUser(id);
+    if(foundUser){ 
+        res.status(200).send(foundUser); 
+        return; 
+    }
+    res.status(404).send(`User #${id} not found in data base`); 
+};
+
