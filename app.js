973d193b4579c049ae4a614113ade699acc867dd
Serg0501 <gavrilenko87sd@gmail.com>
2022-09-24 11:40:31 +0300

    full server package

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
 const express = require('express');
+
 const {userController} = require('./controllers');
-const {validate} = require('./middleware');
+const {validate, errorHandlers} = require('./middleware');
 
 const app = express();
+
+// json data
 app.use(express.json());
+
+//errors
+app.use(errorHandlers.validationEH, errorHandlers.internalServerEH);
 
 // users
 app.get("/users", userController.getAllUsers);
 app.get("/users/:id", userController.getUser);
-
 app.post("/users", validate.validateUserCreate, userController.createNewUser);
-
-app.patch("/users/:id", userController.updateUser);
-
+app.patch("/users/:id", validate.validateUserUpdate, userController.updateUser);
 app.delete("/users/:id", userController.deleteUser);
 
 module.exports = app;
--- middleware/validate.js
+++ middleware/validate.js
@@ -1,21 +1,24 @@
-const yup = require("yup");
+const {userCreateSchema, userUpdateSchema} = require("../utils/validationSchemas");
 
-const userCreateSchema = yup.object({
-    name: yup.string().trim().min(2).max(64).required(),
-    surname: yup.string().trim().min(2).max(64).required(),
-    isMale: yup.boolean().required(),
-    age: yup.number().positive().integer().required()
-});
-
-module.exports.validateUserCreate = (req, res, next) => {
+module.exports.validateUserCreate =  async (req, res, next) => {
   const { body } = req;
-  userCreateSchema
-    .validate(body)
-    .then((validatedUser) => {
+  try{
+    const validatedUser = await userCreateSchema.validate(body);
+    req.body = validatedUser;
+    next();  
+  }catch(error){
+      res.status(400).send(error);
+  }
+};
+
+module.exports.validateUserUpdate = async (req, res, next) => {
+  const { body } = req;
+  try{
+      const validatedUser = await userUpdateSchema.validate(body);
       req.body = validatedUser;
       next();
-    })
-    .catch((error) => {
+  }
+  catch(error){
       res.status(400).send(error);
-    });
+  }
 };
